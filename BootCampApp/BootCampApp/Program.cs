c2093186ee67d46bbf01ef60ea125160f281801e
Abdullah - Al - Naser <naser.bd.edu@gmail.com>
2014-10-18 11:15:21 +0600

    This is Naser's Second Commit

--- BootCampApp/BootCampApp/Program.cs
+++ BootCampApp/BootCampApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BootCampApp.UserInterface;
 
 namespace BootCampApp
 {
@@ -16,7 +17,7 @@ namespace BootCampApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainUI());
         }
     }
 }
